c03054064bc7e7f182c6c725926a197016bb1550
Alexandr <alexandrhub@vk.com>
2024-01-13 15:13:17 +0300

    feat: chat server implementation take 1

---
+++ cmd/grpc-server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/alexandrhub/grpc-chat/pkg/chat_v1"
+)
+
+const grpcPort = 50052
+
+type server struct {
+	pb.UnimplementedChatV1Server
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterChatV1Server(s, &server{})
+
+	log.Printf("Starting gRPC server... on : %v", lis.Addr())
+
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	s.GracefulStop()
+
+	log.Println("Server shutdown gracefully")
+}
